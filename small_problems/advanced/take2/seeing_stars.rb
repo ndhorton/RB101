3192f9e4816aa6e619daba08de561f9677aa7ad3
Nick Horton <nicholashorton1400@gmail.com>
2023-07-03 20:18:50 +0100

    Update seeing_stars.rb

--- small_problems/advanced/take2/seeing_stars.rb
+++ small_problems/advanced/take2/seeing_stars.rb
@@ -97,10 +97,20 @@ def star(grid_size)
   end
 end
 
-# print_line(0, 3)
+# LS solution
+def print_row(grid_size, distance_from_center)
+  number_of_spaces = distance_from_center - 1
+  spaces = ' ' * number_of_spaces
+  output = Array.new(3, '*').join(spaces)
+  puts output.center(grid_size)
+end
+
+def star(grid_size)
+  max_distance = (grid_size - 1) / 2
+  max_distance.downto(1) { |distance| print_row(grid_size, distance) }
+  puts "*" * grid_size
+  1.upto(max_distance)   { |distance| print_row(grid_size, distance) }
+end
 
 star(7)
-
-puts
-
-star(9)
+# star(9)
