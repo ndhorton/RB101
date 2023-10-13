d32114af8fa7f4113588d9d701e40018fd37e565
Nicholas Horton <nicholashorton7@protonmail.com>
2023-10-13 18:21:23 +0100

    Add small problems take 4

---
+++ small_problems/take4/countdown.rb
@@ -0,0 +1,18 @@
+=begin
+Our countdown to launch isn't behaving as expected. Why? Change the code so that our program successfully counts down from 10 to 1.
+
+
+=end
+
+def decrease(counter)
+  counter - 1
+end
+
+counter = 10
+
+10.times do
+  puts counter
+  counter = decrease(counter)
+end
+
+puts 'LAUNCH!'
